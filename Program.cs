3e2eb62435f7272c0d74dcc3a131981d3a29ab81
ursapictura <hsmithfreelance@gmail.com>
2024-07-18 19:09:28 -0500

    All but last two complete.

--- Program.cs
+++ Program.cs
@@ -43,125 +43,140 @@ static List<string> Names(List<Show> shows)
 // 2. Return a list of show names ordered alphabetically.
 static List<string> NamesAlphabetically(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.Name)
+                .Select(s => s.ToString())
+                .ToList();
 }
 
 // 3. Return a list of shows ordered by their IMDB Rating with the highest rated show first.
 static List<Show> ShowsByPopularity(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.ImdbRating).ToList();
 }
 
 // 4. Return a list of shows whose title contains an & character.
 static List<Show> ShowsWithAmpersand(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Where(s => s.Name.ToString().Contains('&'))
+    .ToList();
 }
 
 // 5. Return the most recent year that any of the shows aired.
 static int MostRecentYear(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Max(s => s.EndYear);
 }
 
 // 6. Return the average IMDB rating for all the shows.
 static double AverageRating(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Average(s => s.ImdbRating);
 }
 
 // 7. Return the shows that started and ended in the 90s.
 static List<Show> OnlyInNineties(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Where(s => s.StartYear >= 1990 && s.EndYear < 2000).ToList();
 }
 
 // 8. Return the top three highest rated shows.
 static List<Show> TopThreeByRating(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.ImdbRating).Take(3).ToList();
 }
 
 // 9. Return the shows whose name starts with the word "The".
 static List<Show> TheShows(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Where(s => s.Name[0].ToString() == "T"
+                           && s.Name[1].ToString() == "h"
+                           && s.Name[2].ToString() == "e"
+                           && s.Name[3].ToString() == " ")
+                           .ToList();
 }
 
 // 10. Return all shows except for the lowest rated show.
 static List<Show> AllButWorst(List<Show> shows)
 {
-    throw new NotImplementedException();
+     int minusLowest = shows.Count - 1;
+    return shows.OrderByDescending(s => s.ImdbRating).Take(minusLowest).ToList();
 }
 
 // 11. Return the names of the shows that had fewer than 100 episodes.
 static List<string> FewEpisodes(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Where(s => s.EpisodeCount < 100)
+                .Select(s => s.ToString())
+                .ToList();
 }
 
 // 12. Return all shows ordered by the number of years on air.
 //     Assume the number of years between the start and end years is the number of years the show was on.
 static List<Show> ShowsByDuration(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.EndYear - s.StartYear).ToList();
 }
 
 // 13. Return the names of the comedy shows sorted by IMDB rating.
 static List<string> ComediesByRating(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.ImdbRating)
+                .Where(s => s.Genres.Contains("Comedy"))
+                .Select(s => s.Name)
+                .ToList();
 }
 
 // 14. Return the shows with more than one genre ordered by their starting year.
 static List<Show> WithMultipleGenresByStartYear(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.StartYear)
+                .Where(s => s.Genres.Count > 1)
+                .ToList();
 }
 
 // 15. Return the show with the most episodes.
 static Show MostEpisodes(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderByDescending(s => s.EpisodeCount).Take(1).Single();
 }
 
 // 16. Order the shows by their ending year then return the first 
 //     show that ended on or after the year 2000.
 static Show EndedFirstAfterTheMillennium(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.EndYear).Where(s => s.EndYear >= 2000).Take(1).First();
 }
 
 // 17. Order the shows by rating (highest first) 
 //     and return the first show with genre of drama.
 static Show BestDrama(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderByDescending(s => s.ImdbRating).Where(s => s.Genres.Contains("Drama")).First();
 }
 
 // 18. Return all dramas except for the highest rated.
 static List<Show> AllButBestDrama(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderByDescending(s => s.ImdbRating).Where(s => s.Genres.Contains("Drama")).Skip(1).ToList();
 }
 
 // 19. Return the number of crime shows with an IMDB rating greater than 7.0.
 static int GoodCrimeShows(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.Where(s => s.Genres.Contains("Crime")).Where(s => s.ImdbRating > 7.0).Count();
 }
 
 // 20. Return the first show that ran for more than 10 years 
 //     with an IMDB rating of less than 8.0 ordered alphabetically.
 static Show FirstLongRunningTopRated(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderBy(s => s.Name).Where(s => (s.EndYear - s.StartYear >= 10)).Where(s => s.ImdbRating < 8).First();
 }
 
 // 21. Return the show with the most words in the name.
 static Show WordieastName(List<Show> shows)
 {
-    throw new NotImplementedException();
+    return shows.OrderByDescending(s => s.Name.Length).First();
 }
 
 // 22. Return the names of all shows as a single string seperated by a comma and a space.
@@ -203,59 +218,59 @@ static string AllNamesWithCommasPlsAnd(List<Show> shows)
 class ResultPrinter
 {
     public static void Print(string title, List<Show> shows)
+    {
+        PrintHeaderText(title);
+        foreach (Show show in shows)
         {
-            PrintHeaderText(title);
-            foreach (Show show in shows)
-            {
-                Console.WriteLine(show);
-            }
-
-            Console.WriteLine();
+            Console.WriteLine(show);
         }
+
+        Console.WriteLine();
+    }
 
     public static void Print(string title, List<string> strings)
+    {
+        PrintHeaderText(title);
+        foreach (string str in strings)
         {
-            PrintHeaderText(title);
-            foreach (string str in strings)
-            {
-                Console.WriteLine(str);
-            }
-
-            Console.WriteLine();
+            Console.WriteLine(str);
         }
+
+        Console.WriteLine();
+    }
 
     public static void Print(string title, Show show)
-        {
-            PrintHeaderText(title);
-            Console.WriteLine(show);
-            Console.WriteLine();
-        }
+    {
+        PrintHeaderText(title);
+        Console.WriteLine(show);
+        Console.WriteLine();
+    }
 
     public static void Print(string title, string str)
-        {
-            PrintHeaderText(title);
-            Console.WriteLine(str);
-            Console.WriteLine();
-        }
+    {
+        PrintHeaderText(title);
+        Console.WriteLine(str);
+        Console.WriteLine();
+    }
 
     public static void Print(string title, int number)
-        {
-            PrintHeaderText(title);
-            Console.WriteLine(number);
-            Console.WriteLine();
-        }
+    {
+        PrintHeaderText(title);
+        Console.WriteLine(number);
+        Console.WriteLine();
+    }
 
     public static void Print(string title, double number)
-        {
-            PrintHeaderText(title);
-            Console.WriteLine(number);
-            Console.WriteLine();
-        }
+    {
+        PrintHeaderText(title);
+        Console.WriteLine(number);
+        Console.WriteLine();
+    }
 
     public static void PrintHeaderText(string title)
-        {
-            Console.WriteLine("============================================");
-            Console.WriteLine(title);
+    {
+        Console.WriteLine("============================================");
+        Console.WriteLine(title);
         Console.WriteLine("--------------------------------------------");
     }
 }
